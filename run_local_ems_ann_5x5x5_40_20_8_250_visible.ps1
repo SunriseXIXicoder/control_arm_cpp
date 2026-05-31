6de802d1d98342e12482120729da6d078696602d
yuanchenxi <836909158@qq.com>
2026-05-31 18:03:24 +0800

    Use neck-specific prefix for local ANN run

--- run_local_ems_ann_5x5x5_40_20_8_250_visible.ps1
+++ run_local_ems_ann_5x5x5_40_20_8_250_visible.ps1
@@ -3,10 +3,10 @@ $host.UI.RawUI.WindowTitle = "EMsFEM ANN 5x5x5 40x20x8 250"
 
 Set-Location -LiteralPath $PSScriptRoot
 
-$prefix = "result/ems_ann_5x5x5_40_20_8_np2_250"
+$prefix = "result/ems_ann_5x5x5_neck_40_20_8_np2_250"
 $wslRoot = "/mnt/c/piml_petsc_work/control_arm_cpp"
 
-$bashCommand = "cd `"$wslRoot`" && mkdir -p result && LOG=result/ems_ann_5x5x5_40_20_8_np2_250_launcher.log && : > `"`$LOG`" && exec > >(tee -a `"`$LOG`") 2>&1 && EMS_SUB_N=5 ANN_DIR=../input_5 NELX=40 NELY=20 NELZ=8 ITERS=250 NP=2 WRITE_VTK=true PREFIX=result/ems_ann_5x5x5_40_20_8_np2_250 sh run_wsl_ems_ann_optimize_40_20_8.sh"
+$bashCommand = "cd `"$wslRoot`" && mkdir -p result && LOG=result/ems_ann_5x5x5_neck_40_20_8_np2_250_launcher.log && : > `"`$LOG`" && exec > >(tee -a `"`$LOG`") 2>&1 && EMS_SUB_N=5 ANN_DIR=../input_5 NELX=40 NELY=20 NELZ=8 ITERS=250 NP=2 WRITE_VTK=true PREFIX=result/ems_ann_5x5x5_neck_40_20_8_np2_250 sh run_wsl_ems_ann_optimize_40_20_8.sh"
 
 Write-Host "Starting local EMsFEM/ANN 5x5x5 optimization..."
 Write-Host "Coarse grid: 40 x 20 x 8, iterations: 250, MPI ranks: 2"
